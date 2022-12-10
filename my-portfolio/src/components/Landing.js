597647b68422a01b84cdd9f65421c7d9a3ed1fd2
Dorene07 <dstmarth@gmail.com>
2022-12-09 20:53:06 -0500

    initialized Landing Component

---
+++ my-portfolio/src/components/Landing.js
@@ -0,0 +1,14 @@
+import {React, Component} from 'react';
+import '../App.css'
+
+class Landing extends Component {
+    render() {
+        return(
+            <div>
+                <h5>Hello, I'm Dorene</h5>
+            </div>
+        )
+    }
+}
+
+export default Landing;
--- my-portfolio/src/components/Navbar.js
+++ my-portfolio/src/components/Navbar.js
@@ -1,7 +1,7 @@
-import React from 'react'
+import {React, Component} from 'react'
 import '../App.css'
 
-class Navbar extends React.Component {
+class Navbar extends Component {
     render() {
 
     
