e408db307f1ef55016322de55a09455ff2ff3aa0
Dorene07 <dstmarth@gmail.com>
2022-12-12 00:05:07 -0500

    Projects file created

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Landing from './components/Landing'
+import Projects from './components/Projects'
 
 import './App.css';
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <Navbar />
       <Landing />
+      <Projects />
     </div>
   );
 }
---
+++ my-portfolio/src/components/Projects.js
@@ -0,0 +1,16 @@
+import {React, Component} from 'react'
+import '../App.css'
+
+class Projects extends Component {
+    render() {
+
+    
+    return (
+    <div className="Projects">
+   
+    </div>
+        )
+    }
+}
+
+export default Projects;
