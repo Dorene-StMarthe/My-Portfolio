74afb2163cda6eee57812eb9a04a5c276d00d043
Dorene07 <dstmarth@gmail.com>
2022-12-12 20:41:30 -0500

    button styling

--- my-portfolio/src/components/Landing.js
+++ my-portfolio/src/components/Landing.js
@@ -8,7 +8,8 @@ class Landing extends Component {
             <div className= "Landing">
                 <h5>Dorene St.Marthe </h5>
                 <h3>Dog lover, Coffee enthusiast and Software Developer</h3>
-                <button className= "bg-bermuda-500 hover:bg-bermuda-400 text-white font-bold py-2 px-4 border-b-4 border-bermuda-700 hover:border-blue-500 rounded">View Projects</button>
+                <br></br>
+                <button className= "bg-bermuda hover:bg-cyan text-white font-bold py-2 px-4 border-b-4 border-silver hover:border-cyan rounded">View Projects</button>
             </div>
             
             </>
