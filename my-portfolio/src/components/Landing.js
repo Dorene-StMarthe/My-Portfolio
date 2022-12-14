21a9b4b6202d48200f8fa2dc58510004346183b0
Dorene07 <dstmarth@gmail.com>
2022-12-13 22:36:11 -0500

    leaving in current state

--- my-portfolio/src/components/Landing.js
+++ my-portfolio/src/components/Landing.js
@@ -10,7 +10,9 @@ class Landing extends Component {
                 <h5>Dorene St.Marthe </h5>
                 <h3>Dog lover, Coffee enthusiast and Software Developer</h3>
                 <br></br>
-                <button className= "bg-yellow hover:bg-cyan text-white font-bold py-2 px-4 border-b-4 border-silver hover:border-cyan rounded">View Projects</button>
+                <button 
+                    className= "bg-yellow hover:bg-cyan text-white font-bold py-2 px-4 border-b-4 border-silver hover:border-cyan rounded"
+                    >View Projects</button>
             </div>
             <div>
             <Projects />
