e01c64611be8930bace6bb11b1305304c64b6a86
Dorene07 <dstmarth@gmail.com>
2023-01-20 20:03:58 -0500

    uploaded project description

--- my-portfolio/src/components/Projects.js
+++ my-portfolio/src/components/Projects.js
@@ -1,6 +1,8 @@
 import {React, Component} from 'react'
 import '../App.css'
-
+import wellread from '/Users/dorenest.marthe/Desktop/Personal-Projects/My-Portfolio/my-portfolio/src/images/wellread.png'
+import tictactoe from '/Users/dorenest.marthe/Desktop/Personal-Projects/My-Portfolio/my-portfolio/src/images/tictactoe.png'
+import tindog from '/Users/dorenest.marthe/Desktop/Personal-Projects/My-Portfolio/my-portfolio/src/images/tindog.png'
 class Projects extends Component {
     render() {
 
@@ -8,15 +10,28 @@ class Projects extends Component {
     return (
         <>
          <hr></hr>
-    <div className="Projects">
-   
-       
-            <a href="https://express-bookclub-app.herokuapp.com/wellread" target="_blank" rel="nonreferrer">
+    <div className="ProjectContainer">
+        <div className="Project">
+            <a href="https://express-bookclub-app.herokuapp.com/wellread"  >
                 <img
-                    src="./css/images/wellread.png"
+                    src={wellread}
                     alt="wellread bookclub" />
             </a>
-        
+        </div>
+        <div className="Project">
+            <a href="https://dorene-stmarthe.github.io/TicTacToe/"  >
+                <img
+                    src={tictactoe}
+                    alt="wellread bookclub" />
+            </a>
+        </div>
+        <div className="Project">
+            <a href="https://github.com/Dorene-StMarthe/Tindog.github.io"  >
+                <img
+                    src={tindog}
+                    alt="wellread bookclub" />
+            </a>
+        </div>
     </div>
     </>
         )
